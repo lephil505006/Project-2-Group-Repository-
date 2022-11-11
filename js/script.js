1a2daa2248b7f43afadabf3df3841975f8bda330
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-11-10 20:05:04 -0600

    add ids and add onsubmit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function checkempty() {
-    var fname = document.forms["contactForm"]["fname"].value
-    var lname = document.forms["contactForm"]["lname"].value
+    var fname = document.forms["contactForm"]["firstname"].value
+    var lname = document.forms["contactForm"]["lastname"].value
     var number = document.forms["contactForm"]["phone"].value
     var email = document.forms["contactForm"]["email"].value
     var address = document.forms["contactForm"]["address"].value
