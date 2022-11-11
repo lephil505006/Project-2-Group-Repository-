d4ed1b259ad806b6bc7f2921af02e0c4a51eaa0e
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-11-10 20:01:56 -0600

    add if statement for alert and fix var names

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 function checkempty() {
-    var fname = document.forms["contactForm"]["First Name"].value
-    var lname = document.forms["contactForm"]["Last Name"].value
+    var fname = document.forms["contactForm"]["fname"].value
+    var lname = document.forms["contactForm"]["lname"].value
     var number = document.forms["contactForm"]["phone"].value
     var email = document.forms["contactForm"]["email"].value
-    var address = document.forms["contactForm"]["adress"].value
+    var address = document.forms["contactForm"]["address"].value
 
-
+    if((!fname) || (!lname) || (!number) || (!email) || (!address) ) {
+        alert("Please fill out the required fields");
+        return false;
+    }
 }
