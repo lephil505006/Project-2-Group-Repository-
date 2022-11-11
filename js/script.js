059421c439d6768ea8c4b8436206ebad2b8841f1
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-11-10 19:58:35 -0600

    add variable for checking if contact boxes are empty

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function checkempty() {
+    var fname = document.forms["contactForm"]["First Name"].value
+    var lname = document.forms["contactForm"]["Last Name"].value
+    var number = document.forms["contactForm"]["phone"].value
+    var email = document.forms["contactForm"]["email"].value
+    var address = document.forms["contactForm"]["adress"].value
+
+
+}
